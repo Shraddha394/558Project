6f444621309ff8f644c6dece8c4afd6fa38538d7
Shraddha Yadav <shraddhayadav@Shraddhas-MacBook-Air.local>
2023-10-10 13:18:26 -0400

    third commit

--- RenderScript.R
+++ RenderScript.R
@@ -1,2 +1,3 @@
-rmarkdown::render("/Users/shraddhayadav/Desktop/Fall 2023/ST558/Project 2/558Project/Project2.Rmd", output_format = "github_document", output_file = "README.md")
+rmarkdown::render("/Users/shraddhayadav/Desktop/Fall 2023/ST558/Project 2/558Project/558Project2.Rmd", output_format = "github_document", output_file = "README.md")
+
 
